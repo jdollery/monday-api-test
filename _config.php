e414d90bb900e775a732dd43fd82d7f563ef9981
jdollery <54100876+jdollery@users.noreply.github.com>
2023-10-24 22:36:51 +0100

    Delete _config.php

--- _config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-  $key1 = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjkxMTE0ODcyLCJ1aWQiOjE2MTEyNjIsImlhZCI6IjIwMjAtMTEtMTdUMDg6NTQ6MTguMDAwWiIsInBlciI6Im1lOndyaXRlIiwiYWN0aWQiOjY4NzIwMCwicmduIjoidXNlMSJ9.HzchYQXCp7uUXjgaweircZbnKI-BB2n38S9A-OvH4Qs';
-
-?>
